571e878978d321c38276a14b7ab793c30d8931d7
Chris Geihsler <chris@geihsler.net>
2011-05-16 15:57:42 -0400

    parsing file header

--- lib/pure-sqlite/header.rb
+++ lib/pure-sqlite/header.rb
@@ -1,4 +1,45 @@
 module PureSQLite
   class Header
+
+    STRUCTURE = {
+      well_known_string:  { length: 16  , pattern: 'Z16'  },
+      page_size:          { length: 2   , pattern: 'n'    },
+      write_version:      { length: 1   , pattern: 'C'    },
+      read_version:       { length: 1   , pattern: 'C'    },
+    }
+
+    def initialize(stream)
+      @header = parse_header(stream)
+    end
+
+    def well_known_string
+      @header[:well_known_string]
+    end
+
+    def page_size
+      @header[:page_size]
+    end
+
+    def write_version
+      @header[:write_version]
+    end
+
+    def read_version
+      @header[:read_version]
+    end
+
+    private
+
+    def parse_header(stream)
+      hash = {}
+
+      STRUCTURE.each do |field, opts|
+        field_bytes = stream.read(opts[:length])
+        hash[field] = field_bytes.unpack(opts[:pattern]).first
+      end
+
+      hash
+    end
+
   end
 end
--- spec/header_spec.rb
+++ spec/header_spec.rb
@@ -2,8 +2,27 @@ require "spec_helper"
 
 describe Header do
 
+  before(:all) do
+    test_db_path = File.dirname(__FILE__) + "/resources/test.db"
+    stream = File.open(test_db_path)
+    @header = Header.new(stream)
+    stream.close
+  end
+
   it "should read the magic string" do
-    true.should == false
+    @header.well_known_string.should == "SQLite format 3"
+  end
+
+  it "should read the page size" do
+    @header.page_size.should == 2048
+  end
+
+  it "should read write version" do
+    @header.write_version.should == 1
+  end
+
+  it "should read the read version" do
+    @header.read_version.should == 1
   end
 
 end
