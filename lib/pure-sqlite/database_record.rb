11143cd477981e48f529c73bed40dc839d3e1811
Chris Geihsler <chris@geihsler.net>
2011-05-19 02:25:52 -0400

    reading database record headers

--- lib/pure-sqlite/database_record.rb
+++ lib/pure-sqlite/database_record.rb
@@ -1,12 +1,41 @@
 module PureSQLite
   class DatabaseRecord
 
+    attr_reader :data_types
+
     def initialize(record_size, stream)
-      @entries = VariableLengthInteger.new(stream)
+      @record_length = VariableLengthInteger.new(stream)
+
+      @data_types = []
+      (1..entries).each do
+        type_value = VariableLengthInteger.new(stream).value
+        @data_types << get_data_type_entry(type_value)
+      end
+    end
+
+    def get_data_type_entry(type_value)
+      case(type_value)
+        when 0 then {type: :null  , length: 0}
+        when 1 then {type: :int   , length: 1}
+        when 2 then {type: :int   , length: 2}
+        when 3 then {type: :int   , length: 3}
+        when 4 then {type: :int   , length: 4}
+        when 5 then {type: :int   , length: 6}
+        when 6 then {type: :int   , length: 8}
+        when 7 then {type: :float , length: 8}
+        when 8 then {type: :int   , length: 0}
+        when 9 then {type: :int   , length: 0}
+        else
+          if(type_value.odd? && type_value > 12)
+            {type: :string  , length: (type_value - 13) / 2}
+          else
+            {type: :blob    , length: (type_value - 12) / 2}
+          end
+      end
     end
 
     def entries
-      @entries.value
+      @record_length.value
     end
 
   end
--- spec/database_record_spec.rb
+++ spec/database_record_spec.rb
@@ -17,5 +17,44 @@ describe DatabaseRecord do
       @database_record.entries.should == 7
     end
 
+    it "should have a first data type of string with length 5" do
+      get_data_type(0)[:type].should == :string
+      get_data_type(0)[:length].should == 5
+    end
+
+    it "should have a second data type of string with length 9" do
+      get_data_type(1)[:type].should == :string
+      get_data_type(1)[:length].should == 9
+    end
+
+    it "should have a third data type of string with length 9" do
+      get_data_type(2)[:type].should == :string
+      get_data_type(2)[:length].should == 9
+    end
+
+    it "should have a fourth data type of int with length 5" do
+      get_data_type(3)[:type].should == :int
+      get_data_type(3)[:length].should == 1
+    end
+
+    it "should have a fifth data type of string with length 58" do
+      get_data_type(4)[:type].should == :string
+      get_data_type(4)[:length].should == 58
+    end
+
+    it "should have a sixth data type of blob with length 52" do
+      get_data_type(5)[:type].should == :blob
+      get_data_type(5)[:length].should == 52
+    end
+
+    it "should have a seventh data type of blob with length 52" do
+      get_data_type(6)[:type].should == :string
+      get_data_type(6)[:length].should == 42
+    end
+
+    def get_data_type(index)
+      @database_record.data_types[index]
+    end
+
   end
 end
